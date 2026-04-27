06fef06c08b2aedcebdf3cd1b6d22c7441fffa68
kholoudelamrabti1111 <kholoud.elamrabti@galileiostiglia.edu.it>
2026-04-27 06:46:49 +0000

    libri

--- gestbib/src/libri.c
+++ gestbib/src/libri.c
@@ -5,35 +5,40 @@
 #include <string.h>
 #include "../include/libri.h"
 
+/* Duplica una stringa allocando dinamicamente la memoria necessaria */
 char *duplicaStringa(const char *s) {
-    char *p = malloc(strlen(s) + 1);
-    if (p) strcpy(p, s);
+    char p = malloc(strlen(s) + 1); / +1 per il carattere terminatore '\0' */
+    if (p) strcpy(p, s);             /* Copia la stringa solo se malloc ha avuto successo */
     return p;
 }
 
+/* Raddoppia la capacità del catalogo quando è pieno */
 void espandiCatalogo(CatalogoLibri *catalogo) {
     catalogo->capacita *= 2;
+    /* Ridimensiona l'array dei libri alla nuova capacità */
     catalogo->libri = realloc(catalogo->libri, catalogo->capacita * sizeof(Libro));
 }
 
+/* Inizializza il catalogo con capacità iniziale di 10 libri */
 void inizializzaCatalogo(CatalogoLibri *catalogo) {
     catalogo->capacita = 10;
-    catalogo->num = 0;
-    catalogo->libri = malloc(catalogo->capacita * sizeof(Libro));
+    catalogo->num = 0;                                              /* Nessun libro presente all'inizio */
+    catalogo->libri = malloc(catalogo->capacita * sizeof(Libro));   /* Alloca l'array iniziale */
 }
 
 void aggiungiLibro(CatalogoLibri *catalogo) {
     char buf[256];
 
+    /* Se il catalogo è pieno, espandi prima di aggiungere */
     if (catalogo->num == catalogo->capacita)
         espandiCatalogo(catalogo);
 
-    Libro *l = malloc(sizeof(Libro));
+    Libro l = malloc(sizeof(Libro)); / Alloca temporaneamente un libro */
 
-    l->id = catalogo->num + 1;
+    l->id = catalogo->num + 1; /* L'ID è basato sulla posizione corrente */
 
     printf("Titolo: ");
-    scanf(" %[^\n]", buf);
+    scanf(" %[^\n]", buf); /* Legge l'intera riga inclusi gli spazi */
     l->titolo = duplicaStringa(buf);
 
     printf("Autore: ");
@@ -46,10 +51,10 @@ void aggiungiLibro(CatalogoLibri *catalogo) {
 
     printf("Copie totali: ");
     scanf("%d", &l->copie_totali);
-    l->copie_disponibili = l->copie_totali;
+    l->copie_disponibili = l->copie_totali; /* All'inizio tutte le copie sono disponibili */
 
-    catalogo->libri[catalogo->num] = *l;
-    free(l);
+    catalogo->libri[catalogo->num] = l; / Copia il libro nell'array (per valore) */
+    free(l);                              /* Libera il puntatore temporaneo */
     catalogo->num++;
     printf("Libro aggiunto con successo (ID: %d)\n", catalogo->libri[catalogo->num - 1].id);
 }
@@ -59,14 +64,12 @@ void stampaListaLibri(CatalogoLibri *catalogo) {
         printf("Nessun libro nel catalogo.\n");
         return;
     }
-    printf("\n%-5s %-30s %-20s %-15s %-6s %-6s\n",
-           "ID", "Titolo", "Autore", "Genere", "Totali", "Disp.");
-    printf("----------------------------------------------------------------------\n");
+
     for (int i = 0; i < catalogo->num; i++) {
         Libro *l = &catalogo->libri[i];
-        printf("%-5d %-30s %-20s %-15s %-6d %-6d\n",
+        printf("ID: %d | Titolo: %s | Autore: %s | Genere: %s | Copie: %d/%d\n",
                l->id, l->titolo, l->autore, l->genere,
-               l->copie_totali, l->copie_disponibili);
+               l->copie_disponibili, l->copie_totali);
     }
 }
 
@@ -80,10 +83,11 @@ void modificaLibro(CatalogoLibri *catalogo) {
     printf("ID del libro da modificare: ");
     scanf("%d", &id);
 
+    /* Cerca il libro con l'ID specificato */
     Libro *l = NULL;
     for (int i = 0; i < catalogo->num; i++) {
         if (catalogo->libri[i].id == id) {
-            l = &catalogo->libri[i];
+            l = &catalogo->libri[i]; /* Salva il puntatore al libro trovato */
             break;
         }
     }
@@ -103,7 +107,7 @@ void modificaLibro(CatalogoLibri *catalogo) {
         case 1:
             printf("Nuovo titolo: ");
             scanf(" %[^\n]", buf);
-            free(l->titolo);
+            free(l->titolo);              /* Libera la vecchia stringa prima di sovrascriverla */
             l->titolo = duplicaStringa(buf);
             break;
         case 2:
@@ -139,6 +143,7 @@ void eliminaLibro(CatalogoLibri *catalogo) {
     printf("ID del libro da eliminare: ");
     scanf("%d", &id);
 
+    /* Cerca la posizione del libro nell'array */
     int pos = -1;
     for (int i = 0; i < catalogo->num; i++) {
         if (catalogo->libri[i].id == id) {
@@ -151,10 +156,12 @@ void eliminaLibro(CatalogoLibri *catalogo) {
         return;
     }
 
+    /* Libera la memoria delle stringhe allocate dinamicamente */
     free(catalogo->libri[pos].titolo);
     free(catalogo->libri[pos].autore);
     free(catalogo->libri[pos].genere);
 
+    /* Sposta tutti i libri successivi di una posizione indietro per compattare l'array */
     for (int i = pos; i < catalogo->num - 1; i++)
         catalogo->libri[i] = catalogo->libri[i + 1];
 
@@ -182,6 +189,7 @@ void ricercaLibro(CatalogoLibri *catalogo) {
         Libro *l = &catalogo->libri[i];
         char *campo = NULL;
 
+        /* Seleziona il campo su cui effettuare la ricerca */
         switch (scelta) {
             case 1: campo = l->titolo; break;
             case 2: campo = l->autore; break;
@@ -189,6 +197,7 @@ void ricercaLibro(CatalogoLibri *catalogo) {
             default: printf("Scelta non valida.\n"); return;
         }
 
+        /* strstr restituisce NULL se la parola chiave non è presente nel campo */
         if (strstr(campo, buf) != NULL) {
             printf("ID: %d | %s | %s | %s | copie: %d/%d\n",
                    l->id, l->titolo, l->autore, l->genere,
